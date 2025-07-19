add5fe57f9dab1684ef81b851e3aaecbb9ae0fb8
GitHub Action <action@github.com>
2025-07-19 21:44:31 +0000

    Initialize template repository proper
    Set package name to com.github.borl-jefferson.pearlqol2
    Set project name to PearlQOL2
    Set modid to pearlqol

--- src/main/java/com/github/borl-jefferson/pearlqol2/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/borl-jefferson/pearlqol2/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.borl-jefferson.pearlqol2.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
